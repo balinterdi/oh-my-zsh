df4c40a3c5164ab3fbd599493aaf773ef13b16d3
Gabor Ratky <gabor@secretsaucepartners.com>
2010-09-28 00:43:09 +0200

    Removes minimalistic, updates git and rails plugins

--- plugins/git.plugin.zsh
+++ plugins/git.plugin.zsh
@@ -1,6 +1,5 @@
 # Aliases
-alias g='git'
-alias gst='git status'
+alias gs='git status'
 alias gl='git pull'
 alias gup='git fetch && git rebase'
 alias gp='git push'
@@ -13,10 +12,6 @@ alias gba='git branch -a'
 alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
 
-
-# Git and svn mix
-alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
-
 #
 # Will return the current branch name
 # Usage example: git pull origin $(current_branch)
@@ -26,7 +21,7 @@ function current_branch() {
   echo ${ref#refs/heads/}
 }
 
-# these aliases take advangate of the previous function
+# these aliases take advantage of the previous function
 alias ggpull='git pull origin $(current_branch)'
 alias ggpush='git push origin $(current_branch)'
 alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
