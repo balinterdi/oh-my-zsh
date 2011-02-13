fd48d4bd09769a3d9ea111eea5f4739d1bb48bbc
Gabor Ratky <gabor@secretsaucepartners.com>
2011-02-13 12:10:47 +0100

    Adds code directory shortcuts

---
+++ custom/code.zsh
@@ -0,0 +1,7 @@
+# Shortcuts (e.g. ~dexter)
+dotfiles=/Users/rgabo/.dotfiles
+ohmyzsh=/Users/rgabo/.oh-my-zsh
+dexter=/Users/rgabo/code/sspinc/dexter
+bubbles=/Users/rgabo/code/sspinc/bubbles
+blossom=/Users/rgabo/code/sspinc/blossom
+cartman=/Users/rgabo/code/sspinc/cartman
