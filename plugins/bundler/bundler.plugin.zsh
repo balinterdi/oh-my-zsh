6783a6905b95f2ba93ba525a6ed7cada09693d93
Gabor Ratky <gabor@secretsaucepartners.com>
2011-08-01 10:36:39 +0200

    Add guard to bundled_commands

--- plugins/bundler/bundler.plugin.zsh
+++ plugins/bundler/bundler.plugin.zsh
@@ -6,7 +6,7 @@ alias binit="bundle install && bundle package && echo 'vendor/ruby' >> .gitignor
 
 # The following is based on https://github.com/gma/bundler-exec
 
-bundled_commands=(cap capify cucumber heroku rackup rails rake rspec ruby shotgun spec spork thin unicorn unicorn_rails)
+bundled_commands=(cap capify cucumber guard heroku rackup rails rake rspec ruby shotgun spec spork thin unicorn unicorn_rails)
 
 ## Functions
 
